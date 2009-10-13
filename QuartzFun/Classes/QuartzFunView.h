fae22b39f491725494570aef0beac83eee9b5715
Taegyu Ryu <mordol@gmail.com>
2009-10-13 17:28:04 +0900

    QuartzFunView header추가

--- QuartzFun/Classes/QuartzFunView.h
+++ QuartzFun/Classes/QuartzFunView.h
@@ -7,10 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "Constants.h"
 
 @interface QuartzFunView : UIView {
-
+	CGPoint		firstTouch;
+	CGPoint		lastTouch;
+	UIColor		*currentColor;
+	ShapeType	shapeType;
+	UIImage		*drawImage;
+	BOOL			useRandomColor;
 }
 
+@property CGPoint firstTouch;
+@property CGPoint lastTouch;
+@property (nonatomic, retain) UIColor *currentColor;
+@property ShapeType shapeType;
+@property (nonatomic, retain) UIImage *drawImage;
+@property BOOL useRandomColor;
+
 @end
+
